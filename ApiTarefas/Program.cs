7da40720acfa69b7fbc23410ab181c31702350cd
Ednaldo Tavares <96078863+Miguelito2926@users.noreply.github.com>
2023-11-09 18:02:01 -0300

    Criando o método Put e Delete

--- ApiTarefas/Program.cs
+++ ApiTarefas/Program.cs
@@ -1,48 +1,86 @@
 using Microsoft.EntityFrameworkCore;
 
+// Cria um construtor de aplicativo web usando a classe WebApplication, 
+// passando os argumentos fornecidos à aplicação.
 var builder = WebApplication.CreateBuilder(args);
 
+// Configuração do Swagger para documentação da API.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContex>(opt => opt.UseInMemoryDatabase("TarefasDB"));
+
+// Configuração do DbContext usando o Entity Framework Core para um banco de dados em memória.
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDB"));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configuração do pipeline de solicitação HTTP.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.MapGet("/tarefas", async (AppDbContex db) =>await db.Tarefas.ToListAsync());
 
-app.MapGet("/tarefas/{id}", async (int id, AppDbContex db) =>
-await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+// Mapeamento de endpoints para operações CRUD em 'Tarefas'.
 
-app.MapGet("tarefas/concluidas", async (AppDbContex db) =>
-await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
+// Obtém todas as tarefas.
+app.MapGet("/tarefas", async (AppDbContext db) => await db.Tarefas.ToListAsync());
 
-app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContex db) =>
+// Obtém uma tarefa por ID.
+app.MapGet("/tarefas/{id}", async (int id, AppDbContext db) =>
+    await db.Tarefas.FindAsync(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound());
+
+// Obtém todas as tarefas concluídas.
+app.MapGet("/tarefas/concluidas", async (AppDbContext db) =>
+    await db.Tarefas.Where(t => t.IsConcluida).ToListAsync());
+
+// Adiciona uma nova tarefa.
+app.MapPost("/tarefas", async (Tarefa tarefa, AppDbContext db) =>
 {
     db.Tarefas.Add(tarefa);
     await db.SaveChangesAsync();
     return Results.Created($"/tarefas/{tarefa.Id}", tarefa);
 });
 
+// Atualiza uma tarefa existente por ID.
+app.MapPut("/tarefas/{id}", async (int id, Tarefa inputTarefa, AppDbContext db) =>
+{
+    var tarefa = await db.Tarefas.FindAsync(id);
+    if (tarefa is null) return Results.NotFound();
 
+    tarefa.Nome = inputTarefa.Nome;
+    tarefa.IsConcluida = inputTarefa.IsConcluida;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+// Remove uma tarefa por ID.
+app.MapDelete("/tarefas/{id}", async (int id, AppDbContext db) =>
+{
+    if (await db.Tarefas.FindAsync(id) is Tarefa tarefa)
+    {
+        db.Tarefas.Remove(tarefa);
+        await db.SaveChangesAsync();
+        return Results.Ok(tarefa);
+    }
+    return Results.NotFound();
+});
+
+// Executa o aplicativo.
 app.Run();
 
+// Definição da classe Tarefa.
 class Tarefa
 {
     public int Id { get; set; }
     public string Nome { get; set; }
     public bool IsConcluida { get; set; }
-
 }
 
-class AppDbContex : DbContext
+// Definição da classe AppDbContext que herda de DbContext.
+class AppDbContext : DbContext
 {
-    public AppDbContex(DbContextOptions<AppDbContex> options) : base(options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     { }
 
     public DbSet<Tarefa> Tarefas => Set<Tarefa>();
